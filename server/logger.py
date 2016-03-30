174653cf8f087f0c882d91f46b3dcbdcc14bd4a6
Joon Yee Chuah <jchuah@tacc.utexas.edu>
2016-03-30 14:21:52 -0500

    Menu driven host configuration! No more hunting for your IP. At least on OSX.

--- server/logger.py
+++ server/logger.py
@@ -19,7 +19,8 @@ import socket
 import time
 from threading import Thread
 import sys
-#from multiprocessing import Process
+
+# from multiprocessing import Process
 
 HOST, CONTROL_PORT, DATA_PORT = "192.168.0.100", 9999, 9998
 
@@ -38,9 +39,10 @@ samplerate = "g"
 samplerange = "b"
 streamingData = False
 
-dividers = { "a" : 16380, "b" : 8192, "c" : 4096, "d" : 2048 }
-rates = { "a" : "1 Hz", "b" : "10 Hz", "c" : "25 Hz", "d" : "50 Hz", "e" : "100 Hz", "f" : "200 Hz", "g": "400 Hz" }
-ranges = { "a" : "2G", "b" : "4G", "c" : "8G" , "d" : "16G" }
+dividers = {"a": 16380, "b": 8192, "c": 4096, "d": 2048}
+rates = {"a": "1 Hz", "b": "10 Hz", "c": "25 Hz", "d": "50 Hz", "e": "100 Hz", "f": "200 Hz", "g": "400 Hz"}
+ranges = {"a": "2G", "b": "4G", "c": "8G", "d": "16G"}
+
 
 class AccelerometerClient(object):
     def __init__(self, socket):
@@ -48,15 +50,19 @@ class AccelerometerClient(object):
         self.events = list()
         self.socket = socket
         self.ip = socket.getpeername()[0]
+
     def addEvent(self, timestamp, x, y, z):
-        event = { "timestamp" : timestamp, "x" : x, "y" : y, "z": z }
+        event = {"timestamp": timestamp, "x": x, "y": y, "z": z}
         self.events.append(event)
+
 
 clients = dict()
 
+
 def chunk(l, n):
     for i in xrange(0, len(l), n):
-        yield l[i:i+n]
+        yield l[i:i + n]
+
 
 class DataStreamHandler(SocketServer.BaseRequestHandler):
     def handle(self):
@@ -64,49 +70,52 @@ class DataStreamHandler(SocketServer.BaseRequestHandler):
         client = clients[self.client_address[0]]
         client.events.extend(chunk(data.split(' '), 5))
 
-class AccelerometerHandler(SocketServer.BaseRequestHandler):
-  def setup(self):
-    if not (self.request.getpeername()[0] in clients):
-        self.client = AccelerometerClient(self.request)
-        clients[self.client.ip] = self.client
-        print "New client: " + str(self.client.ip)
-        try:
-            self.request.sendall(str(OPCODE_CONFIGURE + samplerate + samplerange))
-        except Exception:
-            print "Error configuring new client"
-    else:
-        self.client = clients[self.request.getpeername()[0]]
-        self.client.state = STATE_CONNECTED
-        self.client.socket = self.request
-        print "Client reconnected: " + str(self.client.ip)
-        try:
-            self.request.sendall(str(OPCODE_CONFIGURE + samplerate + samplerange))
-        except Exception:
-            print "Error reconfiguring client upon reconnect"
 
-  def handle(self):
-    disconnected = False
-    numTimeouts = 0
-    self.request.settimeout(2)
-    while not quit and not disconnected:
-        try:
-            self.data = self.request.recv(1024).strip()
-            if self.data == OPCODE_KEEPALIVE:
-                # print "Keep alive"
-                self.data = None
-                numTimeouts = 0
-        except Exception as e:
-            if not streamingData:
-                if isinstance(e, socket.timeout):
-                    numTimeouts = numTimeouts + 1
-                    disconnected = numTimeouts > 5
-                else:
-                    disconnected = True
-  def finish(self):
-      if self.client.ip in clients:
-          print str(self.client.ip) + " disconnected"
-          self.client.state = STATE_DISCONNECTED
-          self.request.close()
+class AccelerometerHandler(SocketServer.BaseRequestHandler):
+    def setup(self):
+        if not (self.request.getpeername()[0] in clients):
+            self.client = AccelerometerClient(self.request)
+            clients[self.client.ip] = self.client
+            print "New client: " + str(self.client.ip)
+            try:
+                self.request.sendall(str(OPCODE_CONFIGURE + samplerate + samplerange))
+            except Exception:
+                print "Error configuring new client"
+        else:
+            self.client = clients[self.request.getpeername()[0]]
+            self.client.state = STATE_CONNECTED
+            self.client.socket = self.request
+            print "Client reconnected: " + str(self.client.ip)
+            try:
+                self.request.sendall(str(OPCODE_CONFIGURE + samplerate + samplerange))
+            except Exception:
+                print "Error reconfiguring client upon reconnect"
+
+    def handle(self):
+        disconnected = False
+        numTimeouts = 0
+        self.request.settimeout(2)
+        while not quit and not disconnected:
+            try:
+                self.data = self.request.recv(1024).strip()
+                if self.data == OPCODE_KEEPALIVE:
+                    # print "Keep alive"
+                    self.data = None
+                    numTimeouts = 0
+            except Exception as e:
+                if not streamingData:
+                    if isinstance(e, socket.timeout):
+                        numTimeouts = numTimeouts + 1
+                        disconnected = numTimeouts > 5
+                    else:
+                        disconnected = True
+
+    def finish(self):
+        if self.client.ip in clients:
+            print str(self.client.ip) + " disconnected"
+            self.client.state = STATE_DISCONNECTED
+            self.request.close()
+
 
 def broadcast(command, newstate=None):
     for key in clients:
@@ -118,6 +127,7 @@ def broadcast(command, newstate=None):
         except Exception:
             print "Error sending command to " + str(client.ip)
 
+
 def print_menu():
     print "(a) announce server info"
     print "(c) configure accelerometers"
@@ -126,6 +136,7 @@ def print_menu():
     print "(h) halt data collection"
     print "(w) write data to disk"
     print "(q) quit"
+
 
 def configure():
     global samplerate
@@ -149,6 +160,7 @@ def configure():
     cmdstring = OPCODE_CONFIGURE + samplerate + samplerange
     broadcast(cmdstring)
 
+
 def announce():
     station_id = raw_input("What station ID would you like to broadcast? Capitalization matters! ")
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -156,6 +168,7 @@ def announce():
     s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     data = station_id + "," + str(CONTROL_PORT) + "," + str(DATA_PORT) + "\n"
     s.sendto(data, ('<broadcast>', 9997))
+
 
 def print_status():
     print "Accelerometer settings - " + ranges[samplerange] + " sampling range at " + rates[samplerate]
@@ -168,6 +181,7 @@ def print_status():
         print str(client.ip) + ": " + client.state + " (" + str(len(client.events)) + " events recorded)"
     pass
 
+
 def signal_start():
     broadcast(OPCODE_START, STATE_RUNNING)
     global streamingData
@@ -175,12 +189,14 @@ def signal_start():
     print "Started data collection"
     pass
 
+
 def halt_data_collection():
     broadcast(OPCODE_HALT, STATE_CONNECTED)
     global streamingData
     streamingData = False
     print "Halted data collection"
     pass
+
 
 def write_data():
     filename = raw_input("Enter a filename for the data: ")
@@ -204,57 +220,89 @@ def write_data():
     print "Data written to " + filename
     pass
 
+
 def runServer(host, controlPort, dataPort):
-  print "Starting server on " + str(host) + " with control port " + str(controlPort) + " and data port " + str(dataPort)
-  try:
-      controlServer = SocketServer.TCPServer((host, controlPort), AccelerometerHandler)
-      controlThread = Thread (target=controlServer.serve_forever)
-      controlThread.start()
-      dataServer = SocketServer.UDPServer((host, dataPort), DataStreamHandler)
-      dataThread = Thread(target=dataServer.serve_forever)
-      dataThread.start()
-      quit = False
-      try:
-        while not quit:
-          print_menu()
-          choice = raw_input("Enter choice letter: ").lower()
-          if choice == 'a':
-            announce()
-          elif choice == 'c':
-            configure()
-          elif choice == 'p':
-            print_status()
-          elif choice == 's':
-            signal_start()
-          elif choice == 'h':
-            halt_data_collection()
-          elif choice == 'w':
-            write_data()
-          elif choice == 'q':
+    print "Starting server on " + str(host) + " with control port " + str(controlPort) + " and data port " + str(
+        dataPort)
+    try:
+        controlServer = SocketServer.TCPServer((host, controlPort), AccelerometerHandler)
+        controlThread = Thread(target=controlServer.serve_forever)
+        controlThread.start()
+        dataServer = SocketServer.UDPServer((host, dataPort), DataStreamHandler)
+        dataThread = Thread(target=dataServer.serve_forever)
+        dataThread.start()
+        quit = False
+        try:
+            while not quit:
+                print_menu()
+                choice = raw_input("Enter choice letter: ").lower()
+                if choice == 'a':
+                    announce()
+                elif choice == 'c':
+                    configure()
+                elif choice == 'p':
+                    print_status()
+                elif choice == 's':
+                    signal_start()
+                elif choice == 'h':
+                    halt_data_collection()
+                elif choice == 'w':
+                    write_data()
+                elif choice == 'q':
+                    quit = True
+            print "Calling server shutdown"
+            controlServer.shutdown()
+            dataServer.shutdown()
+            print "Joining thread"
+            controlThread.join()
+            dataThread.join()
+            print "Goodbye!"
+            raise KeyboardInterrupt()
+        except KeyboardInterrupt:
             quit = True
-        print "Calling server shutdown"
-        controlServer.shutdown()
-        dataServer.shutdown()
-        print "Joining thread"
-        controlThread.join()
-        dataThread.join()
-        print "Goodbye!"
-        raise KeyboardInterrupt()
-      except KeyboardInterrupt:
-        quit = True
-        controlServer.shutdown()
-        dataServer.shutdown()
-        controlThread.join()
-        dataThread.join()
-        print "Goodbye!"
-  except:
-      print "Couldn't start server on the specified IP address and port"
+            controlServer.shutdown()
+            dataServer.shutdown()
+            controlThread.join()
+            dataThread.join()
+            print "Goodbye!"
+    except:
+        print "Couldn't start server on the specified IP address and port"
+
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    HOST = sys.argv[1]
-    if len(sys.argv) > 2:
-        CONTROL_PORT = int(sys.argv[2])
-    if len(sys.argv) > 3:
-        DATA_PORT = int(sys.argv[3])
-  runServer(HOST, CONTROL_PORT, DATA_PORT)
+    if len(sys.argv) > 1:
+        HOST = sys.argv[1]
+        if len(sys.argv) > 2:
+            CONTROL_PORT = int(sys.argv[2])
+        if len(sys.argv) > 3:
+            DATA_PORT = int(sys.argv[3])
+    else:
+        # lifted from StackOverflow
+        ipmaybe = ([l for l in (
+            [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [
+                [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in
+                 [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
+        if type(ipmaybe) is str:
+            HOST = ipmaybe
+            print "Your IP address is " + str(HOST)
+        else:
+            print "Choose the network interface for this server: "
+            current = 1
+            for entry in ipmaybe:
+                print "(" + str(current) + ") " + entry
+                current = current + 1
+            choice = raw_input("Choose an entry: ")
+            choice = int(choice)
+            HOST = ipmaybe[choice - 1]
+        cport_input = raw_input("Enter a control port, or leave this blank to use the default of 9999: ")
+        if len(cport_input) <= 0:
+            CONTROL_PORT = 9999
+        else:
+            CONTORL_PORT = int(cport_input)
+        dport_input = raw_input("Enter a data port, or leave this blank to use the default of 9998: ")
+        if len(dport_input) <= 0:
+            DATA_PORT = 9998
+        else:
+            DATA_PORT = int(dport_input)
+
+    runServer(HOST, CONTROL_PORT, DATA_PORT)
